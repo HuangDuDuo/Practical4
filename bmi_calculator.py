ef38ccdb4fe2367806569ac0698b57d8b2d9973f
Apple <huangduduo@hotmail.com>
2025-03-12 15:14:31 +0800

    final

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,9 +1,11 @@
 
 W = float(input("Imput weight:"))
 H = float(input("Imput height:"))
+#calculate BMI
 H2 = H**2
 BMI = W/H2
 print ("Your BMI is:",BMI)
+#feedback
 if BMI > 30:
 	print ("You are obese.")
 elif BMI < 18.5:
--- triangle.py
+++ triangle.py
@@ -1,7 +1,10 @@
-
+#i from 1 to 10
 for i in range(1,11):
+#set  T
 	T = 0
+#add 1, 2, 3... i to T
 	for j in range(1,i+1):
 		T = T+j
+#print result for this time
 	print (T)
 
